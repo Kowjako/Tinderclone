4772602c415346a53552ba37206cc27f86cede48
Wlodzimierz <kowyako@yandex.ru>
2022-11-21 21:46:58 +0100

    Configured route guard + added some styles

--- frontend/client/src/app/_guards/auth.guard.ts
+++ frontend/client/src/app/_guards/auth.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private accService: AccountService, private toastr: ToastrService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accService.currentUser$.pipe(
+      map(user => {
+        if(user) return true;
+        this.toastr.error('You shall not pass!');
+      })
+    );
   }
   
 }
