297daba181aae50e1e5aa58b01852bf5047fe439
Wlodzimierz <kowyako@yandex.ru>
2022-11-27 23:39:09 +0100

    Added member edit component

---
+++ frontend/client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm | undefined
+  member: Member | undefined;
+  user: User | undefined;
+
+  constructor(private accService: AccountService, 
+              private memberService: MembersService, private tt: ToastrService) { 
+    this.accService.currentUser$.pipe(take(1)).subscribe({
+      next: data => this.user = data,
+      error: err => console.log(err)
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    if(!this.user) return;
+    this.memberService.getMember(this.user.username).subscribe({
+      next: resp => this.member = resp,
+      error: err => console.log(err)
+    });
+  }
+
+  updateMember()
+  {
+    console.log(this.member);
+    this.tt.success('Profile updated successfully!');
+    this.editForm?.reset(this.member);
+  }
+}
