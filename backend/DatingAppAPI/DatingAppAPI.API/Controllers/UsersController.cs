03622bf9ac167160b500ee4fa4ee336daedda640
Wlodzimierz <kowyako@yandex.ru>
2022-11-29 23:12:12 +0100

    Added AddPhoto functionality to Users Controller

--- backend/DatingAppAPI/DatingAppAPI.API/Controllers/UsersController.cs
+++ backend/DatingAppAPI/DatingAppAPI.API/Controllers/UsersController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using DatingAppAPI.Application.DTO;
 using DatingAppAPI.Application.Interfaces.Repositories;
+using DatingAppAPI.Application.Interfaces.Services;
+using DatingAppAPI.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -14,11 +17,13 @@ namespace DatingAppAPI.Controllers
     {
         private readonly IUserRepository _repository;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository repo, IMapper mapper)
+        public UsersController(IUserRepository repo, IMapper mapper, IPhotoService photoService)
         {
             _repository = repo;
             _mapper = mapper;
+            _photoService = photoService;
         }
 
         [HttpGet]
@@ -46,6 +51,38 @@ namespace DatingAppAPI.Controllers
             if (await _repository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDTO>> AddPhoto(IFormFile file)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _repository.GetUserByUsernameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if(result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0) photo.IsMain = true;
+
+            user.Photos.Add(photo);
+
+            if (await _repository.SaveAllAsync())
+            {
+                return CreatedAtAction(nameof(GetUserById), 
+                                       new {username = user.UserName},
+                                       _mapper.Map<PhotoDTO>(photo));
+            }
+
+            return BadRequest("Problem adding photo");
         }
     }
 }
