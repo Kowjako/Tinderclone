8b977eacd40331892e8a63ffb5180d6cb0a49bf1
Wlodzimierz <kowyako@yandex.ru>
2022-12-06 12:05:34 +0100

    Added pretty cool message main component !!!

--- backend/DatingAppAPI/DatingAppAPI.Application/Interfaces/Pagination/MessageParams.cs
+++ backend/DatingAppAPI/DatingAppAPI.Application/Interfaces/Pagination/MessageParams.cs
@@ -2,7 +2,7 @@
 {
     public class MessageParams : PaginationParams
     {
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
         public string Container { get; set; } = "Unread";
     }
 }
--- frontend/client/src/app/messages/messages.component.ts
+++ frontend/client/src/app/messages/messages.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faEnvelopeOpen, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessagesService } from '../_services/messages.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
 
-  constructor() { }
+export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container: string = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
+
+  iconEnvelope = faEnvelope;
+  iconEnvelopeOpen = faEnvelopeOpen;
+  iconPaperPlane = faPaperPlane;
+
+  constructor(private msgService: MessagesService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
   }
 
+  loadMessages()
+  {
+    console.log('load msg');
+    this.msgService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: resp => {
+        this.messages = resp.result;
+        console.log(this.messages[0]);
+        this.pagination = resp.pagination;
+      } 
+    })
+  }
+
+  pageChanged(event: any)
+  {
+    if(this.pageNumber !== event.page)
+    {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
