2f7ea580f05573033c80b8f59c7ec781d4be12b4
Wlodzimierz <kowyako@yandex.ru>
2022-12-14 09:37:30 +0100

    Some updates

--- backend/DatingAppAPI/DatingAppAPI.Application/DTO/UserWithRoleDTO.cs
+++ backend/DatingAppAPI/DatingAppAPI.Application/DTO/UserWithRoleDTO.cs
@@ -3,7 +3,7 @@
     public class UserWithRoleDTO
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public List<string> Roles { get; set; }
     }
 }
